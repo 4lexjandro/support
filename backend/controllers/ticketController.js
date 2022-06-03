b076d0089a7066bd0473fa22719ff6723eaa9031
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-06-03 11:48:01 -0400

    ticket model and routes added

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,27 @@
+const asyncHandler= require('express-async-handler')
+
+
+const User = require('../models/userModel')
+const Ticket = require('../models/ticketModel')
+
+
+// @desc Get user Tickets
+//@route GET /api/tickets
+//@access Private
+const getTickets = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'getTickets'})
+})
+
+
+// @desc Create new Tickets
+//@route POST /api/tickets
+//@access Private
+const createTicket = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'createTickets'})
+})
+
+
+module.exports = {
+    getTickets,
+    createTicket,
+}
