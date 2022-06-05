bfd2b44c6dd3306cfb304c139d3623db1c57335e
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-06-05 01:09:59 -0400

    hopefully this works

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.status(200).json({message:'Welcome to the Support Desk'})
-})
+
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
