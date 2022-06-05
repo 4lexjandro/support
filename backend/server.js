11ad2e21ac304016861c91b8bc536b57c6a30815
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2022-06-05 00:58:48 -0400

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+const path = require('path')
+
+
 const express = require('express')
 const colors = require ('colors')
 const dotenv = require('dotenv').config()
@@ -21,6 +24,18 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+//Serve frontend
+if(process.env.NODE_ENV === 'production') {
+    //Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+}
+else {
+    app.get('/', (req, res) => {
+        res.status(200).json({message:'Welcome to the Support Desk'})
+    })
+}
 
 app.use(errorHandler)
 
